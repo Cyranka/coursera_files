b83a5fbe5350628dc65239add9fe0ab9b1302c4a
Harro Cyranka <harrofacyranka@gmail.com>
2019-03-08 19:25:19 -0500

    Simulations

---
+++ applied_time_series/week_3_simulations_1.R
@@ -0,0 +1,16 @@
+remove(list = ls())
+options(stringsAsFactors = FALSE)
+options(scipen = 999)
+
+setwd("/Users/francisco06121988/Desktop/coursera_files/applied_time_series/")
+
+##Graphs of white noise, MA3, and MA5
+par( mfrow=c(3,1));
+plot( arima.sim(n=150, list(order=c(0,0,0) )  ), main="WN", ylab = "");
+plot( arima.sim(n=150, list(ma=c(0.33, 0.33, 0.33)      )  ) , main="MA3",ylab = "");
+plot( arima.sim(n=150, list(ma=c(0.2, 0.2, 0.2, 0.2, 0.2) )  ), main="MA5" ,ylab = "");
+
+##
+par(mfrow = c(1,1)))
+set.seed=1
+(acf(arima.sim(n=1000, model=list(ma=c(0.5,0.5)))))
