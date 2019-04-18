0165e83a2a059edb8c0ac0601d1795169513d56c
Harro Cyranka <harrofacyranka@gmail.com>
2019-04-17 23:14:00 -0400

    Chapter 5 code

---
+++ code_for_hyndman_and_athanasopoulos/chapter_5_code.R
@@ -0,0 +1,7 @@
+remove(list = ls())
+options(stringsAsFactors = FALSE)
+options(scipen = 999)
+
+setwd("/Users/francisco06121988/Desktop/coursera_files/code_for_hyndman_and_athanasopoulos/")
+library(tidyverse);library(fpp2)
+
