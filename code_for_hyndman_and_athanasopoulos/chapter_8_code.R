d1706b04d55fffaca82ce9dbb7ef084c9fc9a6b5
Harro Cyranka <harrofacyranka@gmail.com>
2019-05-21 23:06:37 -0400

    Chapter 8 continuation

--- code_for_hyndman_and_athanasopoulos/chapter_8_code.R
+++ code_for_hyndman_and_athanasopoulos/chapter_8_code.R
@@ -50,3 +50,64 @@ fit %>% forecast(h = 10) %>%
 
 fit %>% forecast(h = 80) %>%
     autoplot(include = 80) ##Notice that the forecasts stabilize with same s.d and mean of the data
+
+# Section 8.5: ACF and PACF -----------------------------------------------
+ggAcf(uschange[,"Consumption"],main = "") ##Autocorrelation plot
+ggPacf(uschange[,"Consumption"],main = "") ##Partial autocorrelation plot
+
+fit2 <- Arima(uschange[,"Consumption"], order = c(3,0,0))
+summary(fit2)
+
+fit3 <- auto.arima(uschange[,"Consumption"],seasonal = FALSE,
+                   stepwise = FALSE, approximation = FALSE) ##This leads to the consideration of other models
+
+summary(fit3)
+
+# Section 8.7: ARIMA modelling --------------------------------------------
+rm(list = ls())
+
+##Remove seasonality so seasonal differencing is unnecessary
+eeadj <- elecequip %>% stl(s.window = "periodic") %>% seasadj()
+
+autoplot(elecequip, alpha =0.5, color = "gray") + 
+    autolayer(eeadj) ##Plot the series
+
+eeadj %>% diff(lag = 1) %>% ggtsdisplay(main = "") ##Take first differences
+eeadj %>% diff(lag = 1) %>% urca::ur.kpss() %>% summary() ##This also confirms the data
+
+(fit <- Arima(eeadj, order = c(3,1,1))) ##Took first differences, but no seasonal differences
+
+checkresiduals(fit) ##Ljung-box text confirms residuals are white-noise
+autoplot(forecast(fit)) ##c = 0 and first differences, so forecasts will converge to a constant
+
+# Section 8.9: Sarima model -----------------------------------------------
+rm(list = ls())
+
+autoplot(euretail) + ylab("Retail index") + xlab("Year")
+
+euretail %>% diff(lag = 4) %>% ggtsdisplay() ##Remove seasonal difference
+euretail %>% diff(lag = 4) %>% diff() %>% ggtsdisplay()
+
+euretail %>%
+    Arima(order = c(0,1,1),seasonal = c(0,1,1)) %>%
+    residuals() %>% ggtsdisplay() ###
+
+fit3 <- Arima(euretail, order = c(0,1,3), seasonal = c(0,1,1))
+checkresiduals(fit3)
+
+fit3 %>% forecast(h = 12) %>% autoplot()
+
+# Section 8.9: Cortecosteroid drug sales in Australia ---------------------
+lh02 <- log(h02) ##Stabilize variance
+cbind("H02 sales (million scripts)" = h02,
+      "Log H02 sales" = lh02) %>%
+    autoplot(facets = TRUE) + 
+    xlab("Year") + ylab("")
+
+lh02 %>% diff(lag = 12) %>%
+    ggtsdisplay(xlab = "Year",
+                main = "Seasonally differenced H02 scripts")
+
+fit <- Arima(h02, order = c(3,0,1),seasonal = c(0,1,2),lambda = 0) ##Log transform with lambda = 0
+summary(fit)
+checkresiduals(fit, lag = 36)
